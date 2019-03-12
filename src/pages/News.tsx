263750ddc72c999f9ebd8006b4f67853885669a3
fargustian <aleksfers@icloud.com>
2019-03-12 22:01:53 +0500

    Implemented routing type checks

---
+++ src/pages/News.tsx
@@ -0,0 +1,12 @@
+import { RouteComponentProps } from '@reach/router'
+import React from 'react'
+
+const News: React.FC<RouteComponentProps> = () => {
+  return (
+    <div className="news">
+      <p>News</p>
+    </div>
+  )
+}
+
+export default News
